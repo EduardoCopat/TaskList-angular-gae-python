2d89ca64672e46352fa999d41dc77c3e52f938a7
eduardocopat <ecopat@gmail.com>
2015-09-02 21:11:57 -0300

    order by date

--- lib/DateEncoder.py
+++
@@ -1,9 +0,0 @@
-import json
-import datetime
-
-
-class DateEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, datetime.date):
-            return obj.isoformat()
-        return json.JSONEncoder.default(self, obj)
--- lib/TaskRecord.py
+++
@@ -1,6 +0,0 @@
-from google.appengine.ext import ndb
-
-class TaskRecord(ndb.Model):
-    title = ndb.StringProperty()
-    description = ndb.StringProperty(indexed=False)
-    date = ndb.DateTimeProperty(auto_now_add=True)
